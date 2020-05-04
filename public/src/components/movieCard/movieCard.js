9948e1e1e4e87bfb445a30ef1fe9a261496aa2b2
Aria Sadeghi <ariasadeghi@gmail.com>
2020-05-03 19:16:45 -0700

    render movie cards, need styling

--- public/src/components/movieCard/movieCard.js
+++ public/src/components/movieCard/movieCard.js
@@ -1,14 +1,57 @@
 import React, {useContext, useEffect} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import MovieContext from '../../utils/MovieContext';
-import axios from 'axios';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
 
 function MovieCard(){
-const {handleQueryMovies, queryMovies} = useContext(MovieContext);
-useEffect(()=> {
-   
-})
+const {queryMovies} = useContext(MovieContext);
+  const classes = useStyles();
+
 return(
-    <h1>Hello, World</h1>
+    <Container>
+    {queryMovies.map(movie => 
+        <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={movie.Poster}
+            title={movie.Title}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {movie.Title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+             {movie.Year}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            View on IMDB
+          </Button>
+        </CardActions>
+      </Card>
+        )}
+    </Container>
+   
 )
 }
 
